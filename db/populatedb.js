122d501f41078d341d49aa86b399c8e4546c7cca
Lionel <lpei1900@gmail.com>
2024-10-30 17:09:56 -0400

    Fixed names in populate db

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,16 +1,17 @@
+require('dotenv').config(); 
 const { Client } = require("pg");
 
 const SQL = `
 CREATE TABLE IF NOT EXISTS genres (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL UNIQUE,
-  image_url VARCHAR(255)
+  name VARCHAR(255) NOT NULL UNIQUE,
+  image VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS publishers (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL UNIQUE,
-  image_url VARCHAR(255)
+  image VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS games (
@@ -19,20 +20,20 @@ CREATE TABLE IF NOT EXISTS games (
   release_date DATE,
   genre_id INTEGER REFERENCES genres(id),
   publisher_id INTEGER REFERENCES publishers(id),
-  image_url VARCHAR(255)
+  image VARCHAR(255)
 );
 
 
-INSERT INTO genres (title, image_url) VALUES
+INSERT INTO genres (name, image) VALUES
   ('Sport', 'https://pixune.com/wp-content/uploads/2023/10/FIFA.png.webp'),
   ('Puzzle', 'https://pixune.com/wp-content/uploads/2023/10/Puzzle.png.webp');
 
-INSERT INTO publishers (name, image_url) VALUES
+INSERT INTO publishers (name, image) VALUES
   ('EA Sports', 'https://images.igdb.com/igdb/image/upload/t_logo_med/cl4vv.png'),
   ('Rockstar Games ', 'https://images.igdb.com/igdb/image/upload/t_logo_med/cl8g0.png');
 
-INSERT INTO games (title, release_date, genre_id, publisher_id, image_url) VALUES
-  ('FIFA 21', '10/8/2020', 1, 1, 'https://images.igdb.com/igdb/image/upload/t_cover_big/co3wm2.webp');
+INSERT INTO games (title, release_date, genre_id, publisher_id, image) VALUES
+  ('FIFA 21', '2020-8-10', 1, 1, 'https://images.igdb.com/igdb/image/upload/t_cover_big/co3wm2.webp');
 
 `;
 
@@ -40,7 +41,7 @@ async function main() {
   console.log("Seeding database...");
 
   const client = new Client({
-    connectionString: "postgresql://<role_name>:<role_password>@localhost:5432/top_users", // Update with your credentials
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, 
   });
 
   try {
