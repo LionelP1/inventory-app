f7c2747d8380ff76ea61c1d154ee024d3764c1b9
Lionel <lpei1900@gmail.com>
2024-11-02 00:02:49 -0400

    Created the styles for the add buttons

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/games', controllers.renderFilteredGames);
 router.get('/games/search', controllers.renderFilteredGames);
 router.get('/games/form', controllers.renderGameForm);
-router.post('/games/form',controllers.submitGameForm);
+router.post('/games/form', validators.validateGame, controllers.submitGameForm);
 router.get('/games/:id', controllers.renderGameDetails);
 router.delete('/games/:id', controllers.deleteGame);
 module.exports = router;
--- routes/publisherRoutes.js
+++ routes/publisherRoutes.js
@@ -4,9 +4,8 @@ const validators = require('../controllers/validators');
 const router = express.Router();
 
 router.get('/publishers/form', controllers.renderPublisherForm);
-router.post('/publishers/form', controllers.submitPublisherForm);
+router.post('/publishers/form', validators.validatePublisher, controllers.submitPublisherForm);
 router.delete('/publishers/:id', controllers.deletePublisher);
 router.get('/publishers/:id', controllers.renderPublisherDetails);
 
 module.exports = router;
-
