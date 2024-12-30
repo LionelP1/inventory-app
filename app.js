2abe9819936f223e77aedb8f194c166e5fa59ed3
Lionel <lpei1900@gmail.com>
2024-12-30 17:19:59 -0500

    Updated app.js

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use("/", publisherRoutes);
 app.use("/", genreRoutes);
 app.use("/", inventoryRoutes);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
